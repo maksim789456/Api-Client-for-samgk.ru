b5332d2a3491bcbd5bf4a47f37de96b0194480cc
TheCrazyWolf <74318176+TheCrazyWolf@users.noreply.github.com>
2025-01-06 16:39:46 +0400

    Rollback commits from Rusik (#33)
    * Revert "[WIP] Рефакторинг от рузика v3.0.1  (#28)"
    
    
    Co-authored-by: maksim789456 <git@capybaraesttravy.ru>

--- ClientSamgkOutputResponse/Implementation/Schedule/ResultOutResultOutScheduleFromDate.cs
+++ ClientSamgkOutputResponse/Implementation/Schedule/ResultOutResultOutScheduleFromDate.cs
@@ -5,21 +5,8 @@ namespace ClientSamgkOutputResponse.Implementation.Schedule;
 
 public class ResultOutResultOutScheduleFromDate : IResultOutScheduleFromDate
 {
-    public ResultOutResultOutScheduleFromDate(DateOnly date, ScheduleSearchType searchType, string idValue,
-        ScheduleCallType callType = ScheduleCallType.Standart)
-    {
-        Date = date;
-        SearchType = searchType;
-        IdValue = idValue;
-        CallType = callType;
-    }
-
-    public ResultOutResultOutScheduleFromDate()
-    {
-    }
-
     public DateOnly Date { get; set; }
-    public IList<IResultOutLesson> Lessons { get; set; } = [];
+    public IList<IResultOutLesson> Lessons { get; set; } = new List<IResultOutLesson>();
     public ScheduleSearchType SearchType { get; set; }
     public string IdValue { get; set; } = string.Empty;
     public ScheduleCallType CallType { get; set; }
