b8f87dccd04c9fe5f63061d7e1b1429a120069c9
TheCrazyWolf <74318176+TheCrazyWolf@users.noreply.github.com>
2025-01-10 14:09:19 +0400

    Merge pull request #35
    * Модель для передачи настроек кеша
    
    * Комментарии
    
    * Реализация метода установки параметров
    
    * Возможность установки сроки жизни кеша через конструктор ClientSamgkApi

---
+++ ClientSamgk.Models.Params/Implementations/Cache/CacheOptions.cs
@@ -0,0 +1,23 @@
+﻿using ClientSamgk.Models.Params.Interfaces.Cache;
+
+namespace ClientSamgk.Models.Params.Implementations.Cache;
+
+public class CacheOptions : ICacheOptions
+{
+    public CacheOptions()
+    {
+        
+    }
+
+    public CacheOptions(int lifeTimeInMinutesForCommon, int 
+        lifeTimeInMinutesLong, int lifeTimeInMinutesShort)
+    {
+        LifeTimeCommonObjects = lifeTimeInMinutesForCommon;
+        LifeTimeObjectsForLong = lifeTimeInMinutesLong;
+        LifeTimeObjectsForShort = lifeTimeInMinutesShort;
+    }
+
+    public int LifeTimeCommonObjects { get; } = 2880;
+    public int LifeTimeObjectsForLong { get; } = 43200;
+    public int LifeTimeObjectsForShort { get; } = 10;
+}
---
+++ ClientSamgk.Models.Params/Interfaces/Cache/ICacheOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientSamgk.Models.Params.Interfaces.Cache;
+
+public interface ICacheOptions
+{
+    /// <summary>
+    /// Срок жизни общих объектов (списки преподавателей, групп, кабинетов.
+    /// в минутах
+    /// </summary>
+    int LifeTimeCommonObjects { get; }
+    /// <summary>
+    /// Срок жизни долгих и не меняющихся объектов (например прошедшее расписание)
+    /// в минутах
+    /// </summary>
+    int LifeTimeObjectsForLong { get;  }
+    /// <summary>
+    /// Срок жизни часто меняющихся объектов (например, расписание на сегодня и последующие дни)
+    /// в минутах
+    /// </summary>
+    int LifeTimeObjectsForShort { get;  }
+}
